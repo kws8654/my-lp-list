e3d040f4b7933cc0ee2d5af1ce18f78c4e519a61
qws3433 <kws8654@naver.com>
2021-12-07 18:51:05 +0900

    almost done

--- script.js
+++ script.js
@@ -1,61 +1,61 @@
-$(".startstop").click(function () {
-    $(this).toggleClass("active");
-    $('.turntable').toggleClass("active");
+$('.startstop').click(function () {
+  $(this).toggleClass('active');
+  $('.turntable').toggleClass('active');
 });
 
-$(".startstop").click(function () {
-    $(this).toggleClass("active");
-    $('.rec').toggleClass("active");
+$('.startstop').click(function () {
+  $(this).toggleClass('active');
+  $('.rec').toggleClass('active');
 });
 
-var num = 1;
+let num = 1;
 function nextLp(index) {
-    var image = document.querySelector(".rec");
-    var audio = document.querySelector(".audioPlay");
-    // let rec = document.querySelector(".rec").getAttribute('src');
-    let title = document.querySelector(".console");
+  const image = document.querySelector('.rec');
+  const audio = document.querySelector('.audioPlay');
+  // let rec = document.querySelector(".rec").getAttribute('src');
+  let title = document.querySelector('.console');
 
-    if (index) {
-        if (num == 4) return;
-        num++;
-    } else {
-        if (num == 1) return;
-        num--;
-    }
+  if (index) {
+    if (num == 5) return;
+    num++;
+  } else {
+    if (num == 1) return;
+    num--;
+  }
 
-    image.setAttribute("src", "rec" + num + ".png");
+  image.setAttribute('src', 'rec' + num + '.png');
 
-    if (audio.paused) {
-        audio.setAttribute("src", "aud" + num + ".mp3");
-    } else {
-        audio.setAttribute("src", "aud" + num + ".mp3");
-        audio.play();
-    }
-    if (image.getAttribute('src') === "rec1.png") {
-        title.innerText = num + "." + "Ohayo"
-    } else if (image.getAttribute('src') === "rec2.png") {
-        title.innerText = num + "." + "4walls"
-    } else if (image.getAttribute('src') === "rec3.png") {
-        title.innerText = num + "." + "sunny"
-    } else if (image.getAttribute('src') === "rec4.png") {
-        title.innerText = num + "." + "sunny"
-    }
+  if (audio.paused) {
+    audio.setAttribute('src', 'aud' + num + '.mp3');
+  } else {
+    audio.setAttribute('src', 'aud' + num + '.mp3');
+    audio.play();
+  }
+  if (image.getAttribute('src') === 'rec1.png') {
+    title.innerText = '연극이 끝난 후';
+  } else if (image.getAttribute('src') === 'rec2.png') {
+    title.innerText = 'Seoul city pop';
+  } else if (image.getAttribute('src') === 'rec3.png') {
+    title.innerText = '네온';
+  } else if (image.getAttribute('src') === 'rec4.png') {
+    title.innerText = '운전만 해';
+  } else if (image.getAttribute('src') === 'rec5.png') {
+    title.innerText = 'Summer man';
+  }
 }
-
 
 function playLp() {
-    var audio = document.querySelector(".audioPlay");
-    let rec = document.querySelector(".rec").getAttribute('src');
-    let title = document.querySelector(".console");
+  const audio = document.querySelector('.audioPlay');
+  let rec = document.querySelector('.rec').getAttribute('src');
+  let title = document.querySelector('.console');
 
-    if (audio.paused) {
-        audio.play();
-    } else {
-        audio.pause();
-        audio.currentTime = 0
-    }
-    if (rec === "./rec1.png") {
-        title.innerText = "1. Ohayo";
-    }
+  if (audio.paused) {
+    audio.play();
+  } else {
+    audio.pause();
+    audio.currentTime = 0;
+  }
+  if (rec === './rec1.png') {
+    title.innerText = '연극이 끝난 후';
+  }
 }
-
